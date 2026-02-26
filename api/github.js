6f5baf4377006e96df566d0e7f0a252a76ad4557
LORD PREMO <cypherx599@gmail.com>
2026-02-26 08:01:26 +0300

    Update github.js

--- api/github.js
+++ api/github.js
@@ -3,12 +3,8 @@ export default async function handler(req, res) {
   if (!q) return res.status(400).json({ error: "Missing q parameter" });
 
   const url = `https://api.github.com/search/repositories?q=${encodeURIComponent(q)}`;
-
   const r = await fetch(url);
   const data = await r.json();
 
-  res.json({
-    query: q,
-    results: data.items
-  });
+  res.json({ query: q, results: data.items });
 }
