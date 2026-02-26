26d89b618a12f47eb584bb8e89f649436a360c1b
LORD PREMO <cypherx599@gmail.com>
2026-02-26 07:28:32 +0300

    Create wiki.js

---
+++ api/wiki.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const q = req.query.q;
+  if (!q) return res.status(400).json({ error: "Missing q parameter" });
+
+  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=${encodeURIComponent(q)}`;
+
+  const r = await fetch(url);
+  const data = await r.json();
+
+  res.json({
+    query: q,
+    results: data.query.search
+  });
+}
