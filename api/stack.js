266ec5d220cee1184e571f4ab3f3bcaadd30c120
LORD PREMO <cypherx599@gmail.com>
2026-02-26 07:31:40 +0300

    Create stack.js

---
+++ api/stack.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const q = req.query.q;
+  if (!q) return res.status(400).json({ error: "Missing q parameter" });
+
+  const url = `https://api.stackexchange.com/2.3/search?order=desc&sort=activity&intitle=${encodeURIComponent(q)}&site=stackoverflow`;
+
+  const r = await fetch(url);
+  const data = await r.json();
+
+  res.json({
+    query: q,
+    results: data.items
+  });
+}
