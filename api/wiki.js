157ce4b0d4f187a93b4e4ebf807ae21de08752a1
LORD PREMO <cypherx599@gmail.com>
2026-02-26 07:58:01 +0300

    Update wiki.js

--- api/wiki.js
+++ api/wiki.js
@@ -3,12 +3,8 @@ export default async function handler(req, res) {
   if (!q) return res.status(400).json({ error: "Missing q parameter" });
 
   const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=${encodeURIComponent(q)}`;
-
   const r = await fetch(url);
   const data = await r.json();
 
-  res.json({
-    query: q,
-    results: data.query.search
-  });
+  res.json({ query: q, results: data.query.search });
 }
