c5e9c909d87a074370fb7ac3fb2db795de1aac73
LORD PREMO <cypherx599@gmail.com>
2026-02-26 07:29:09 +0300

    Create ddg.js

---
+++ api/ddg.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const q = req.query.q;
+  if (!q) return res.status(400).json({ error: "Missing q parameter" });
+
+  const url = `https://api.duckduckgo.com/?q=${encodeURIComponent(q)}&format=json&no_redirect=1&no_html=1`;
+
+  const r = await fetch(url);
+  const data = await r.json();
+
+  res.json({
+    query: q,
+    abstract: data.Abstract,
+    heading: data.Heading,
+    related: data.RelatedTopics
+  });
+    }
