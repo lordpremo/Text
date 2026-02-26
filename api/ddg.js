d58535ae111ad224160cc5598db42019790aa37e
LORD PREMO <cypherx599@gmail.com>
2026-02-26 07:58:44 +0300

    Update ddg.js

--- api/ddg.js
+++ api/ddg.js
@@ -3,7 +3,6 @@ export default async function handler(req, res) {
   if (!q) return res.status(400).json({ error: "Missing q parameter" });
 
   const url = `https://api.duckduckgo.com/?q=${encodeURIComponent(q)}&format=json&no_redirect=1&no_html=1`;
-
   const r = await fetch(url);
   const data = await r.json();
 
@@ -13,4 +12,4 @@ export default async function handler(req, res) {
     heading: data.Heading,
     related: data.RelatedTopics
   });
-    }
+}
